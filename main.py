45c2c8a54cf178a534f011ff78eec2464fb3a509
DuncanByrneUT <duncanbyrne7254@gmail.com>
2024-12-30 19:28:01 -0500

    added code to make the story print a cohesive story in the terminal, the user can replace the words in the <bracket> with whatever they like. i also changed the story in the txt file, thew madlib generator is now finished

--- main.py
+++ main.py
@@ -25,4 +25,6 @@ for word in words:
     answer = input("enter a word for " + word + ": ")
     answers[word] = answer
 
-print(answers)
+for word in words:
+    story = story.replace(word, answers[word])
+print(story)
